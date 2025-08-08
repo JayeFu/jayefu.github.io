6d3d0523440d34e66d90935d9b7d86349fbd1f67
JayeFu <jiawei.taurus@gmail.com>
2025-08-08 19:04:34 +0000

    Deploying to gh-pages from @ JayeFu/jayefu.github.io@3b435ebe32f2a1719ac81867b5f833b01aa3408e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -55,14 +55,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%6A%69%66%30%31%37@%75%63%73%64.%65%64%75", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://www.linkedin.com/in/jiawei-fu-b6687b200", "_blank");
         },
       },{
         id: 'social-rss',
@@ -76,14 +76,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=5E_2M1UAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
