520283b4ef0eccc8f70eeabe2761c3776b1c380b
JayeFu <jiawei.taurus@gmail.com>
2026-05-09 23:16:15 +0000

    Deploying to gh-pages from @ JayeFu/jayefu.github.io@a3bd263a688ee8aa72e44f89e00df1630bca22ba 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -65,7 +65,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/cv.pdf", "_blank");
+          window.open("/assets/pdf/jfu_cv.pdf", "_blank");
         },
       },{
         id: 'social-email',
